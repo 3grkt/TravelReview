91f5869253c63784c4bd279096fa21277cd02089
Nha Nguyen <ntuannha@gmail.com>
2021-01-12 10:42:06 -0600

    Created services for Photo and Token.

---
+++ TravelReview.Services/TokenService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using TravelReview.Models.Account;
+
+namespace TravelReview.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        private readonly string _issuer;
+
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            _issuer = config["Jwt:Issuer"];
+        }
+
+        public string CreateToken(ApplicationUserIdentity user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId)
+            }
+        }
+    }
+}
